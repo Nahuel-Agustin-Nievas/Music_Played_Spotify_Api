4770e53f96d06914ce6804f8edf8333d69458ba3
Nahuel Nievas <nievasnahuel@gmail.com>
2023-03-15 21:33:47 +0100

    readme added

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
 
     }
     today = datetime.datetime.now()
-    yesterday =  today - datetime.timedelta(days=2)
+    yesterday =  today - datetime.timedelta(days=1)
     yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
 
     r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix_timestamp), headers=headers)
